91bb6ae1ca404e843c6cab280d440256603594f0
Vijayavimala <vijayavimala@gmail.com>
2017-01-10 10:15:29 +0530

    Local repository is created

---
+++ WebdriverGoogleTest/src/test/java/Title/NewTest.java
@@ -0,0 +1,29 @@
+package Title;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	public  WebDriver driver;
+		  
+	
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "E:\\Chrome\\chromedriver.exe");
+	  driver=new ChromeDriver();
+	  driver.get("https://www.google.com/");
+	  String text =driver.getTitle();
+	  System.out.println("Title of Google - "+ text);
+	  Assert.assertEquals(text, "Google");
+	  driver.close();
+	  
+  }
+    
+  
+
+}
